0b9eabe72e65a29cd22aa9955aa5b0d8925a5ff4
Shon <miceroot@gmail.com>
2023-11-07 20:40:52 +0200

    Bun_APIs

---
+++ index.ts
@@ -0,0 +1,16 @@
+import figlet from "figlet"
+import text from "./text.txt"
+
+const server = Bun.serve({
+	port: 3535,
+	fetch(req){
+		const body = figlet.textSync("Bun!");
+
+		return new Response(body);
+		return new Response("Bun!");
+	},
+});
+
+console.log(text);
+console.log(Bun.version);
+console.log(`listening on http://localhost:${server.port} . . .`);
