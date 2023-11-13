ee676a7ba4293c12597d93748a01e7a10bd57dc1
MiceX <32219039+shonber@users.noreply.github.com>
2023-11-13 19:25:05 +0200

    Delete react.tsx

--- react.tsx
+++
@@ -1,9 +0,0 @@
-function Component(props: {message: string}) {
-  return (
-    <body>
-      <h1 style={{color: 'red'}}>{props.message}</h1>
-    </body>
-  );
-}
-
-console.log(<Component message="Hello world!" />);
